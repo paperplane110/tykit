e909a507a098c356abbd804a938d38b15a008ad6
tyyuan <tyyuan@aibee.com>
2021-04-02 17:47:19 +0800

    fix init bug

---
+++ examples/pbrange.py
@@ -0,0 +1,14 @@
+'''
+Description: 
+version: 
+Author: TianyuYuan
+Date: 2021-04-02 17:33:38
+LastEditors: TianyuYuan
+LastEditTime: 2021-04-02 17:35:27
+'''
+from tykit import pb_range
+from time import sleep
+
+# use pb_range just like range()
+for i in pb_range(50):
+   sleep(0.001)
