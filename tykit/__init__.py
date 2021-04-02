7cd38e27cc6afad214b9b9870bd611f890143650
tyyuan <tyyuan@aibee.com>
2021-04-02 18:50:38 +0800

    update init

--- tykit/__init__.py
+++ tykit/__init__.py
@@ -4,11 +4,18 @@ version:
 Author: TianyuYuan
 Date: 2021-04-02 15:40:39
 LastEditors: TianyuYuan
-LastEditTime: 2021-04-02 18:38:00
+LastEditTime: 2021-04-02 18:49:18
 '''
 name = "tykit"
-from tykit import *
+from tykit import (
+    pb_range,
+    pb_iter,
+    pb_multi_thread,
+    pb_multi_thread_partial,
+    RLog,
+    ProgressBar
+    )
 
 if __name__ == "__main__":
     rlog = RLog()
-    rlog.start('sdlf')
+    rlog.start('hello world')
